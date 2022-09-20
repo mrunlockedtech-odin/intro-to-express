270678f36a606a3125cb28f5c8b0e5810a283500
mrunlockedtech <jrbhrn@gmail.com>
2022-09-19 20:27:37 -0500

    Add student data

---
+++ data/student-data.js
@@ -0,0 +1,12 @@
+const students = [
+  {name: 'Timothy Jonesworth', passing:true, _id:12380},
+  {name: 'Bradley Withersworth', passing:false, _id:12330},
+  {name: 'Joe Kerr', passing:true, _id:12320},
+  {name: 'Billiam Gates', passing:false, _id:12343},
+  {name: 'Jefferson Bezos', passing:false, _id:12375},
+  {name: 'Elongated Muskrat', passing:true, _id:12393},
+]
+
+export {
+  students
+}
